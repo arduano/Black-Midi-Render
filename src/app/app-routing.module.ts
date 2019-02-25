3efd635a5a7686303bec1bb3a12f6d6721f63ee2
arduano <leonid.shchurov@gmail.com>
2019-02-26 01:00:07 +1100

    Removed old stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
-import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { MainComponent } from './core/main/main.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy } from '@angular/router';
 import { StartPageComponent } from './pages/start-page/start-page.component';
-import { WorksPageComponent } from './pages/works-page/works-page.component';
-import { SetupPageComponent } from './pages/setup-page/setup-page.component';
 import { BenchmarksPageComponent } from './pages/benchmarks-page/benchmarks-page.component';
 
 
@@ -14,9 +11,6 @@ const routes: Routes = [
   {
     path: 'Black-Midi-Render', component: MainComponent, children: [
       { path: 'start', component: StartPageComponent },
-      { path: 'about', component: AboutPageComponent },
-      { path: 'works', component: WorksPageComponent },
-      { path: 'setup', component: SetupPageComponent },
       { path: 'benchmarks', component: BenchmarksPageComponent},
     ]
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartPageComponent } from './pages/start-page/start-page.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { MainComponent } from './core/main/main.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { WorksPageComponent } from './pages/works-page/works-page.component';
-import { SetupPageComponent } from './pages/setup-page/setup-page.component';
 import { PageScrollerComponent } from './core/page-scroller/page-scroller.component';
 import { SocialMediaComponent } from './misc/social-media/social-media.component';
 import { MatButtonModule, MatCheckboxModule, MatCardModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatRadioModule, MatProgressBarModule,} from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { GoogleFormsService } from './services/google-forms/google-forms.service';
 import { HttpClient } from 'selenium-webdriver/http';
 import { HttpClientModule } from '@angular/common/http';
 import { BenchmarksPageComponent } from './pages/benchmarks-page/benchmarks-page.component';
@@ -25,10 +21,7 @@ import { BenchmarkComponent } from './misc/benchmark/benchmark.component';
   declarations: [
     AppComponent,
     StartPageComponent,
-    AboutPageComponent,
     MainComponent,
-    WorksPageComponent,
-    SetupPageComponent,
     PageScrollerComponent,
     SocialMediaComponent,
     BenchmarksPageComponent,
