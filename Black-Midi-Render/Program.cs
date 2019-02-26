1d72fb07a277212abcb294169d378c6e3b8e5091
arduano <leonid.shchurov@gmail.com>
2019-02-27 08:29:51 +1100

    Testing render plugin reflection

--- Black-Midi-Render/Program.cs
+++ Black-Midi-Render/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,6 +16,12 @@ namespace Black_Midi_Render
         [STAThread]
         static void Main(string[] args)
         {
+            //var DLL = Assembly.LoadFile(Path.GetFullPath("Plugins\\ClassicRender.dll"));
+            //foreach (Type type in DLL.GetExportedTypes())
+            //{
+            //    dynamic c = Activator.CreateInstance(type);
+            //    c.HelloWorld("asdf");
+            //}
             Console.Title = "Black Midi Render";
             Application app = new Application();
             app.Run(new MainWindow());
